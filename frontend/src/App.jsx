77966fd1beedbada9b4cd780b6ffdbdffeb5411f
Pjotr Straathof <pjotrjacobstraathof@gmail.com>
2025-06-23 16:58:03 +0200

    created form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import NotFound from './pages/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+
+function LogOut() {
+  localStorage.clear();
+  return <Navigate to="/login" />;
+}
+
+function RegisterAndLogout() {
+  localStorage.clear();
+  return <Navigate to="/register" />;
+}
 
 function App() {
-
   return (
-    <>
-      
-    </>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<LogOut />} />
+          <Route path="/register" element={<RegisterAndLogout />} />
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
   )
 }
 
